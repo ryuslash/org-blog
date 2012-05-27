2ab6a393d1cbc7a38cb435aabd27ec659d5837f0
Tom Willemsen <tom@ryuslash.org>
2012-05-27 11:57:07 +0200

    Change version number
    Version 1.18+1 might be more descriptive than 1.18.1, but package.el
    makes it 1.18alpha1 and I don't much care for that.

--- org-blog.el
+++ org-blog.el
@@ -6,7 +6,7 @@
 ;; Maintainer: Tom Willemsen <tom@ryuslash.org>
 ;; Original Author: David O'Toole <dto@gnu.org>
 ;; Keywords: hypermedia, tools
-;; Version: 1.18+1
+;; Version: 1.18.1
 ;;
 ;; This file is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
